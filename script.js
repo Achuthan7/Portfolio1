9c2f40f4a33ba645c99cc830ae37a3e2b19a44ea
Achuthan7 <141254121+Achuthan7@users.noreply.github.com>
2024-05-28 19:18:18 +0530

    Update script.js
    added animations

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const scroll=ScrollReveal({
+    reset: true,
+    duration:3000,
+    distance:'60px',
+})
+scroll.reveal('.image-transform',{delay:500,origin:'top'})
+scroll.reveal('.text-transform',{delay:300, origin:'left'})
 function clicker(){
     var menu=document.getElementById('dropmenu');
     var bt=document.getElementById('btn');
+   
     if(menu.classList.contains('hidden')){
+       
         menu.classList.remove('hidden');
         bt.classList.add('border');
     }
     else{
+       
         menu.classList.add('hidden');
         bt.classList.remove('border')
     }
